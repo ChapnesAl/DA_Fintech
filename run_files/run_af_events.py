0d0af875c5adb29b2ab53692631b0abf91b5bee5
ChapnesAI <job.chap@icloud.com>
2022-12-07 16:18:19 +0600

    - create file to run event_af requests

---
+++ run_files/run_af_events.py
@@ -0,0 +1,19 @@
+from af_tokens import token_tezbol, id_andr_tezbol
+from Appsflyer_api.af_events_data.ask_af_events import AF_events_data
+
+""" Date """
+from_date = '2022-12-06'
+to_date = '2022-12-06'
+
+
+""" Token and app ID"""
+app_id = id_andr_tezbol
+token = token_tezbol
+
+# app_id = id_andr_kreditomat_kz
+# token = token_kreditomat_kz
+
+
+
+if __name__ == '__main__':
+    print(AF_events_data(app_id, token, to_date, from_date).get_loan_rejected())
